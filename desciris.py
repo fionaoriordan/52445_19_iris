adf1d4b7dc7cdea560d6035870f2b3885572d9b0
Fiona O'Riordan <fiona_oriordan@hotmail.com>
2019-04-02 12:44:04 +0100

    Draft of desciris.py the python script describing the iris data added

---
+++ desciris.py
@@ -0,0 +1,36 @@
+# Fiona O'Riordan 28 March 2019 
+# Project Iris Data Set
+# Describe the Iris Data Set
+# Adapted from:
+# https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+
+# import the pandas library and rename as pd 
+import pandas as pd
+# import sys library so that we can use function sys.argv 
+import sys
+
+# load the iris dataset. 
+# In order to be able to work offline I have downloaded the set and added to the 52445_19_IRIS folder instead.
+# url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class'] 
+
+# read in the iris.csv now saved locally, assign the names created above to the data as the attribute names/column headings.
+iris = pd.read_csv("iris.csv",names=names)
+
+# Create a statistical summary 
+irisdesc = iris.describe()
+print(irisdesc,'\n')
+
+
+# create a txt file called "descirisoutput.txt" and write the iris description to the file
+with open("descirisoutput.csv","w") as f:
+      f.write(str(irisdesc))
+      
+
+
+
+
+ 
+
+
+ 
