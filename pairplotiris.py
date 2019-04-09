ec5765594be98012e315a9eb657e147a439a6e3d
Fiona O'Riordan <fiona_oriordan@hotmail.com>
2019-04-09 16:54:11 +0100

    Updated pairplotiris.py to include graph title

--- pairplotiris.py
+++ pairplotiris.py
@@ -3,6 +3,9 @@
 # Script to show scatterplots for all of the variable combinations in the iris data set and histograms on the diagonal showing the distribution of each variable individually  with the iris class 'species' represented by difference in color.
 # Adapted from: 
 # https://seaborn.pydata.org/tutorial/distributions.html#visualizing-pairwise-relationships-in-a-dataset
+# https://www.datacamp.com/community/tutorials/seaborn-python-tutorial#title
+# https://seaborn.pydata.org/generated/seaborn.pairplot.html
+# https://stackoverflow.com/questions/29813694/how-to-add-a-title-to-seaborn-facet-plot
 # import the seaborn library so that we can use seaborn functions and rename as sns 
 import seaborn as sns
 # import the matplotlib library functions below  and rename as plt
@@ -10,7 +13,11 @@ import matplotlib.pyplot as plt
 
 # load the iris data set from seaborn
 iris = sns.load_dataset("iris")
-# create a pairplot for the iris dataset with the iris data set class 'species' determing color.
-sns.pairplot(iris, hue='species')
+# create a pairplot for the iris dataset with the iris data set class 'species' determing color and call it g
+g = sns.pairplot(iris, hue='species') 
+# adjust the height of the title so it does not overlap the graph
+plt.subplots_adjust(top=0.9)   
+# create a chart subtitle.
+g.fig.suptitle("Iris: Histogram & Scatter with Class by Color")
 # output the figure created
 plt.show()
