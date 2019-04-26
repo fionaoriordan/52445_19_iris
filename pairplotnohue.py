e98fc80ab72905bfb40376ccec8ff81bd4cadbb4
Fiona O'Riordan <fiona_oriordan@hotmail.com>
2019-04-26 21:55:51 +0100

    updating references and comments

--- pairplotnohue.py
+++ pairplotnohue.py
@@ -2,11 +2,11 @@
 # Iris Data Project
 # Script to show scatterplots for all of the variable combinations in the iris data set and histograms on the diagonal showing the distribution of each variable individually  WITHOUT the iris class 'species' represented by difference in color.
 # Adapted from: 
-# https://seaborn.pydata.org/tutorial/distributions.html#visualizing-pairwise-relationships-in-a-dataset
-# https://www.datacamp.com/community/tutorials/seaborn-python-tutorial#title
-# https://seaborn.pydata.org/generated/seaborn.pairplot.html
-# https://stackoverflow.com/questions/29813694/how-to-add-a-title-to-seaborn-facet-plot
-# https://stackoverflow.com/questions/47542104/how-to-control-the-legend-in-seaborn-python
+# https://seaborn.pydata.org/tutorial/distributions.html#visualizing-pairwise-relationships-in-a-dataset [23]
+# https://www.datacamp.com/community/tutorials/seaborn-python-tutorial#title [24]
+# https://seaborn.pydata.org/generated/seaborn.pairplot.html [25]
+# https://stackoverflow.com/questions/29813694/how-to-add-a-title-to-seaborn-facet-plot [26]
+# https://stackoverflow.com/questions/47542104/how-to-control-the-legend-in-seaborn-python [27]
 # import the seaborn library so that we can use seaborn functions and rename as sns 
 import seaborn as sns
 # import the matplotlib library functions below  and rename as plt
